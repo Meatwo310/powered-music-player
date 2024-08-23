59d0dd3c306483da8bb5bc3ba7c006e31df463f5
Meatwo310 <72017364+Meatwo310@users.noreply.github.com>
2024-08-23 20:17:50 +0900

    とりあえずテンプレ

---
+++ src/main/java/io/github/meatwo310/poweredmusicplayer/PoweredMusicPlayer.java
@@ -0,0 +1,14 @@
+package io.github.meatwo310.poweredmusicplayer;
+
+import com.mojang.logging.LogUtils;
+import net.minecraftforge.fml.common.Mod;
+import org.slf4j.Logger;
+
+@Mod(PoweredMusicPlayer.MODID)
+public class PoweredMusicPlayer {
+    public static final String MODID = "poweredmusicplayer";
+    private static final Logger LOGGER = LogUtils.getLogger();
+
+    public PoweredMusicPlayer() {
+    }
+}
